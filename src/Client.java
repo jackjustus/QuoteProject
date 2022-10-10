e467ce55f8db7642567546459b60d6d738fc0cfa
jackjustus <jackjustushome@gmail.com>
2022-10-10 14:46:37 -0700

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class Client extends PApplet {
+
+    @Override
+    public void settings() {
+        fullScreen();
+        super.settings();
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        super.draw();
+    }
+
+
+    public static void main(String[] args) {
+        PApplet.main("Client");
+
+
+    }
+}
